f7b6131b493ff58ac8d375968f7b52f5de2ad810
archery <omrigamer100@gmail.com>
2024-11-22 01:20:38 +0200

    added option for eval rules;

--- src/utils/tree_rules.ts
+++ src/utils/tree_rules.ts
@@ -1,5 +1,21 @@
-const tree_rules_eval = async (report_id: string, report_parent:number) => {
-    console.log(`report_id: ${report_id}, report_parent: ${report_parent}`)
-}
+import pool from "../database/db";
+
+const tree_rules_eval = async (report_id: string, report_parent: number) => {
+  //   console.log(`report_id: ${report_id}, report_parent: ${report_parent}`);
+
+  const nodes_and_reports = await pool.query(
+    "select * from nodes where parent = $1",
+    [report_parent]
+  );
+
+  const rules = await pool.query(
+    "select * from rules where parent_node_id = $1",
+    [report_parent]
+  );
+
+  console.log(nodes_and_reports.rows);
+
+  console.log(rules.rows);
+};
 
 export default tree_rules_eval;
