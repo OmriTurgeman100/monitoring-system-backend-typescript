deacf709c5dfdf598fd9a892e7506a05f54a3abc
archery <omrigamer100@gmail.com>
2024-11-21 21:05:19 +0200

    added route in index.ts main file

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ const port: number = 80;
 
 app.use(express.json());
 app.use("/api/v1/nodes", nodeRouter);
+app.use("/api/v1/reports", reportRouter);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
