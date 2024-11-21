4ab2fca8595dbd928b0d398b2ab642d6c974d94c
archery <omrigamer100@gmail.com>
2024-11-21 19:31:28 +0200

    made a scripts file

---
+++ src/database/scripts.sql
@@ -0,0 +1,27 @@
+create table nodes (
+	node_id SERIAL PRIMARY KEY,
+	parent INTEGER REFERENCES nodes(node_id) ON DELETE CASCADE,
+	title VARCHAR(50) NOT NULL,
+	description VARCHAR(50) NOT NULL,
+	status VARCHAR(50) DEFAULT 'expired',
+    excluded VARCHAR(50) DEFAULT 'false',
+	time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+create table reports (
+	id SERIAL PRIMARY KEY,
+	report_id VARCHAR(50) NOT NULL,
+	parent INTEGER REFERENCES nodes(node_id) ON DELETE CASCADE,
+	title VARCHAR(50) NOT NULL,
+	description VARCHAR(50) NOT NULL,
+	value INTEGER,
+	time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE rules (  
+    rule_id SERIAL PRIMARY KEY,
+    parent_node_id INTEGER REFERENCES nodes(node_id) ON DELETE CASCADE,
+    conditions JSONB NOT NULL,  
+    action VARCHAR(50) NOT NULL,
+    time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
