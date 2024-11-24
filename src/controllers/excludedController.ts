ec1522c5698f1c5c03f121a0c42ea9f8c4764fba
archery <omrigamer100@gmail.com>
2024-11-24 22:23:30 +0200

    delete excluded route

--- src/controllers/excludedController.ts
+++ src/controllers/excludedController.ts
@@ -18,3 +18,21 @@ export const set_excluded_report: RequestHandler = async (req, res, next) => {
     });
   }
 };
+
+export const delete_excluded_report: RequestHandler = async (req, res, next) => {
+  try {
+    const report_id: string = req.body.report_id;
+    const parent: number = req.body.parent;
+
+    const excluded = pool.query(
+      "update reports set excluded = 'true' where report_id = $1 and parent = $2",
+      [report_id, parent]
+    );
+
+    res.status(201).json({ message: "The POST request was successful" });
+  } catch (error) {
+    res.status(400).json({
+      message: error,
+    });
+  }
+};
