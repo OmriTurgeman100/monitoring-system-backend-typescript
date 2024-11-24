33b79780ac84745ba70bd77c85d5a892ab2e8e59
archery <omrigamer100@gmail.com>
2024-11-24 22:16:19 +0200

    building the excluded queries

--- src/controllers/excludedController.ts
+++ src/controllers/excludedController.ts
@@ -1,3 +1,18 @@
 import { RequestHandler } from "express";
 import pool from "../database/db";
 
+export const set_excluded_report: RequestHandler = async (req, res, next) => {
+  try {
+    const parent: number = req.body.parent;
+    const report_id: string = req.body.report_id;
+    
+    const excluded = pool.query(
+      "update reports set excluded = 'false' where report_id = $1 and parent = $2",
+      []
+    );
+  } catch (error) {
+    res.status(400).json({
+      message: error,
+    });
+  }
+};
