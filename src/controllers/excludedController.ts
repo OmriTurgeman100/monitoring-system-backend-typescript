2dca75df643073b6a7d0a3c89c10369b6be8d366
archery <omrigamer100@gmail.com>
2024-11-24 22:22:28 +0200

    set excluded function

--- src/controllers/excludedController.ts
+++ src/controllers/excludedController.ts
@@ -3,13 +3,15 @@ import pool from "../database/db";
 
 export const set_excluded_report: RequestHandler = async (req, res, next) => {
   try {
-    const parent: number = req.body.parent;
     const report_id: string = req.body.report_id;
+    const parent: number = req.body.parent;
 
     const excluded = pool.query(
-      "update reports set excluded = 'false' where report_id = $1 and parent = $2",
-      []
+      "update reports set excluded = 'true' where report_id = $1 and parent = $2",
+      [report_id, parent]
     );
+
+    res.status(201).json({ message: "The POST request was successful" });
   } catch (error) {
     res.status(400).json({
       message: error,
