4a4a300f7c8f608573ec5f49d77fa586c50c9210
archery <omrigamer100@gmail.com>
2024-11-28 20:38:09 +0200

    report can be related only to 1 parent.

--- src/controllers/reportController.ts
+++ src/controllers/reportController.ts
@@ -1,6 +1,7 @@
 import { RequestHandler } from "express";
 import pool from "../database/db";
 import tree_rules_eval from "../utils/tree_rules";
+import { Request, Response, NextFunction } from "express";
 
 export const get_distinct_reports: RequestHandler = async (req, res, next) => {
   try {
@@ -29,17 +30,39 @@ export const post_reports: RequestHandler = async (req, res, next) => {
           const report_parent: number = report.parent;
           const excluded: string = report.excluded;
 
-          const created_report = await pool.query(
+          await pool.query(
             "insert into reports (report_id, parent, title, description, value, excluded) values ($1, $2, $3, $4, $5, $6) RETURNING *;",
             [report_id, report_parent, title, description, value, excluded]
           );
 
-          await tree_rules_eval(report_parent);
+          // await tree_rules_eval(report_parent);
         }
       }
     }
 
-    const created_report = await pool.query(
+    if (parent) {
+      const distinct_reports = await pool.query(
+        "select distinct on (parent) parent, report_id, title, excluded from reports where report_id = $1 and parent is not null",
+        [report_id]
+      );
+
+      if (distinct_reports.rows.length > 0) {
+        for (const report of distinct_reports.rows) {
+          const report_parent: number = report.parent;
+
+          console.log(report_parent);
+
+          if (report_parent != parent) {
+            res.status(400).json({
+              message: "report can only be under 1 parent.",
+            });
+            return; 
+          }
+        }
+      }
+    }
+
+    await pool.query(
       "insert into reports (report_id, parent, title, description, value) values ($1, $2, $3, $4, $5) RETURNING *;",
       [report_id, parent, title, description, value]
     );
@@ -51,3 +74,4 @@ export const post_reports: RequestHandler = async (req, res, next) => {
     });
   }
 };
+
